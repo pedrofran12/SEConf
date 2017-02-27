d46a390220efef124615654d4976788a4069a4b3
Pedro Oliveira <pedrofran.oliveira@hotmail.com>
2017-02-27 14:06:55 +0000

    fixed key

---
+++ pm-ws_juddi/src/main/java/pm/Key.java
@@ -0,0 +1,80 @@
+
+package pm;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for docUserPair complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="docUserPair">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="documentId" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="userId" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "key", propOrder = {
+    "key", "algorithm"
+})
+
+public class Key {
+
+    @XmlElement(required = true)
+    protected String key;
+    @XmlElement(required = true)
+    protected String algorithm;
+
+
+    /**
+     * Gets the value of the key property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public java.security.Key getKey() {
+    	// decode the base64 encoded string
+    	byte[] decodedKey = Base64.getDecoder().decode(key);
+    	// rebuild key using SecretKeySpec
+    	return new SecretKeySpec(decodedKey, 0, decodedKey.length, algorithm);
+    }
+
+    /**
+     * Sets the value of the key property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     * @throws NoSuchAlgorithmException 
+     *     
+     */
+    public void setKey(java.security.Key key, String algorithm) throws NoSuchAlgorithmException {
+    	this.algorithm = algorithm;
+    	// create new key
+    	SecretKey secretKey = KeyGenerator.getInstance(algorithm).generateKey();
+    	// get base64 encoded version of the key
+    	this.key = Base64.getEncoder().encodeToString(secretKey.getEncoded());
+    }
+}
--- pm-ws_juddi/src/main/java/pm/PasswordManagerImpl.java
+++ pm-ws_juddi/src/main/java/pm/PasswordManagerImpl.java
@@ -1,18 +1,31 @@
 package pm;
 
-import java.security.Key;
-
+import javax.jws.WebMethod;
 import javax.jws.WebService;
 
 @WebService(endpointInterface = "pm.PasswordManager")
-public class PasswordManagerImpl implements PasswordManager {
+public class PasswordManagerImpl implements PasswordManager, java.io.Serializable{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PasswordManagerImpl(){
+		
+	}
+	
+	@WebMethod(exclude = true)
 	public void register(Key publicKey){
 		
 	}
+	
+	@WebMethod(exclude = true)
 	public void put(Key publicKey, byte[] domain, byte[] username, byte[] password){
 		
 	}
+	
+	@WebMethod(exclude = true)
 	public byte[] get(Key publicKey, byte[] domain, byte[] username){
 		return null;
 	}
