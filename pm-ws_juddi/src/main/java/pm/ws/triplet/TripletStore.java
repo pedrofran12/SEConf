0fc2fbabe1287cff976b3ab328f6a697fe91ed2c
luisrafael1995 <luis_rafael_1995@hotmail.com>
2017-03-01 14:36:44 +0000

    added sincronization support

--- pm-ws_juddi/src/main/java/pm/ws/triplet/TripletStore.java
+++ pm-ws_juddi/src/main/java/pm/ws/triplet/TripletStore.java
@@ -2,29 +2,44 @@ package pm.ws.triplet;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class TripletStore {
 	private final List<Triplet> store;
+	private final Lock wLock;
+	private final Lock rLock;
 	
 	public TripletStore() {
 		store = new ArrayList<>();
+		ReentrantReadWriteLock rrwl = new ReentrantReadWriteLock(true);
+		wLock = rrwl.writeLock();
+		rLock = rrwl.readLock();
 	}
 	
 	public void put(byte[] domain, byte[] username, byte[] password) {
+		wLock.lock();
 		Triplet t = getTriplet(domain, username);
 		if (t == null) {
 			store.add(new Triplet(domain, username, password));
 		} else {
 			t.setPassword(password);
 		}
+		wLock.unlock();
 	}
 	
 	public byte[] get(byte[] domain, byte[] username) {
+		rLock.lock();
+		byte[] passwd = null;
 		Triplet t = getTriplet(domain, username);
-		if (t == null) {
+		if (t != null) {
+			passwd = t.getPassword();
+		}
+		rLock.unlock();
+		if (passwd != null) {
 			// throw new UnknowUsernameDomainException();
 		}
-		return t.getPassword();
+		return passwd;
 	}
 	
 	private Triplet getTriplet(byte[] domain, byte[] username) {
