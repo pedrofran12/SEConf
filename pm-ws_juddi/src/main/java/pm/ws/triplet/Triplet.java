0a077570387c39e40e67644aaed3c45722e6a0a7
Jose Pereira <j.luis.pereira@tecnico.ulisboa.pt>
2017-03-01 23:20:57 +0000

    Added InvalidPasswordException

--- pm-ws_juddi/src/main/java/pm/ws/triplet/Triplet.java
+++ pm-ws_juddi/src/main/java/pm/ws/triplet/Triplet.java
@@ -1,13 +1,14 @@
 package pm.ws.triplet;
 
 import java.io.Serializable;
+import pm.exception.*;
 
 public class Triplet extends TripletHeader implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private byte[] password;
 
-	public Triplet(byte[] dmn, byte[] uname, byte[] passwd) {
+	public Triplet(byte[] dmn, byte[] uname, byte[] passwd) throws InvalidPasswordException  {
 		super(dmn, uname);
 		setPassword(passwd);
 	}
@@ -16,9 +17,9 @@ public class Triplet extends TripletHeader implements Serializable {
 		return password;
 	}
 	
-	public void setPassword(byte[] passwd) {
+	public void setPassword(byte[] passwd) throws InvalidPasswordException {
 		if (passwd == null) {
-			//throw new InvalidPasswordException();
+			throw new InvalidPasswordException();
 		}
 		password = passwd;
 	}
