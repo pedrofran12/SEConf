b5f580cf20f0d52c6fc1bdea5a91c1c7ca9895cc
Jose Pereira <j.luis.pereira@tecnico.ulisboa.pt>
2017-03-03 18:38:35 +0000

    Added exception

--- pm-ws_juddi/src/main/java/pm/handler/HandlerFunction.java
+++ pm-ws_juddi/src/main/java/pm/handler/HandlerFunction.java
@@ -1,39 +1,38 @@
 package pm.handler;
 
-import javax.xml.soap.SOAPBody;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import pm.exception.InvalidMessageDigestException;
 
 public class HandlerFunction {
 
     // - DIGEST - //
-    public static MessageDigest Object2Hash(MessageDigest md, Object obj) throws Exception {// needs
-                                                                                            // revision
+    public static MessageDigest object2Hash(MessageDigest md, Object obj) throws NoSuchAlgorithmException, IOException {
+        
         if (md == null) {
             md = MessageDigest.getInstance("SHA-256");
         }
-
-        md.update(Object2Bytes(obj));
-
+        md.update(object2Bytes(obj));
         return md;
     }
 
-    public static byte[] digestMessage(MessageDigest md) throws Exception {
+    public static byte[] digestMessage(MessageDigest md) throws InvalidMessageDigestException {
 
         if (md == null) {
-            throw new Exception(); // needs to be corrected
+            throw new InvalidMessageDigestException(); // needs to be corrected
         }
-
         return md.digest();
     }
 
-    private static byte[] Object2Bytes(Object obj) throws Exception {
+    private static byte[] object2Bytes(Object obj) throws IOException{
 
         // Convert Object To bytes!
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -44,6 +43,7 @@ public class HandlerFunction {
         return bos.toByteArray();
     }
 
+    //useless but if needed is created!
     private static Object byte2Object(byte[] byt) throws Exception {
         ByteArrayInputStream bis = new ByteArrayInputStream(byt);
         ObjectInput in = new ObjectInputStream(bis);
@@ -52,6 +52,4 @@ public class HandlerFunction {
     }
     
     // - KEY PART - 
-    
-    
 }
